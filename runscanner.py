a7886ff0644a2e64a283ed6c5018b932894cd630
Gaurab Bhattacharjee <gaurabb@hotmail.com>
2017-07-01 23:36:43 -0700

    Results display changes

--- runscanner.py
+++ runscanner.py
@@ -4,6 +4,8 @@ class RunScan:
 	objScanUrlsInFile =  scan("secureheaderscanner/url_list.txt")
 	result = objScanUrlsInFile.scanUrlsInFile()
 	for info in result:
-		print("{0}: {1}".format(info,result[info]))
+		print("{0}:".format(info))
+		for count in result[info]:
+			print("\t{0}: {1}".format(count, result[info][count]))
 
 
